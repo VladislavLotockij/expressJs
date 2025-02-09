70106d56b64888bb782679cf914cc8ad96cbf2bf
Vladislav <vladislavlotockij1@gmail.com>
2025-02-09 12:24:41 +0200

    Added task model schema to database

---
+++ src/models/taskModel.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const taskSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: false,
+  },
+  status: {
+    type: String,
+    enum: ["pending", "comleted"],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+});
+
+const Task = mongoose.model("Task", taskSchema);
+module.exports(Task);
